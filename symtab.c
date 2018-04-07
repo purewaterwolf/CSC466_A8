83db695a9eb03dcd2abd0a015b30eb14a358fddb
Caitlin Rudolph <brooke_rudolph@hotmail.com>
2018-04-07 17:15:17 -0500

    4/7 Update symtab.c

--- symtab.c
+++ symtab.c
@@ -36,3 +36,47 @@ int intab( char *s)
 
 }
 
+
+int addtype( char *s, int t)
+{
+ int i, loc = -1;
+ for ( i = 1; i <= nsym; ++i)
+ {
+   if ( strcmp(symtab[i].sname, s) == 0)
+    loc = i;
+ }
+ if (loc > 0)
+  {
+//   printf("Set type %s to %d\n", s, t);
+   symtab[loc].stype = t;
+  }
+ else
+ {
+   printf("Unable to set type %s to %d\n", s, t);
+ }
+}
+
+
+int gettype( char *s)
+{
+ int t = -1;
+ int i, loc = -1;
+ for ( i = 1; i <= nsym; ++i)
+ {
+   if ( strcmp(symtab[i].sname, s) == 0)
+    loc = i;
+ }
+ if (loc > 0)
+  {
+   t = symtab[loc].stype;
+//   printf("Get type for %s to %d\n", s, t);
+  }
+ if (loc <= 0)
+   printf("gettype var %s not found\n", s);
+ else if (t < 0)
+   printf("gettype var %s has bad type %d\n", s, t);
+ else
+//   printf("gettype var %s has type %d\n", s, t);
+
+ return t;
+}
