9cfdfc03689bd09217d643f551fc17ce39a2eb0f
Arielle Kent <arielle.julane@gmail.com>
2018-04-06 18:00:22 -0700

    Add symtab.c

---
+++ symtab.c
@@ -0,0 +1,38 @@
+struct stelem
+{
+ char sname[25];
+ int  stype;
+};
+typedef struct stelem entry;
+
+
+entry symtab[100];
+int nsym;
+
+
+void addtab( char *s)
+{
+ nsym++;
+ strcpy( symtab[nsym].sname, s);
+ symtab[nsym].stype = -1;
+}
+
+void showtab()
+{
+ int i;
+ for (i = 1; i <= nsym; ++i)
+   printf("%d: %s %d\n", i, symtab[i].sname, symtab[i].stype);
+}
+
+int intab( char *s)
+{
+ int i;
+ for ( i = 1; i <= nsym; ++i)
+ {
+   if ( strcmp(symtab[i].sname, s) == 0)
+    return 1;
+ }
+ return 0;
+
+}
+
